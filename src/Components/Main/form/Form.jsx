02a93cd3912da8e46726e921e81f2bdec31f7d33
Fernando Andres Poblete <andrespbt14@hotmail.com>
2023-04-05 15:37:06 -0300

    feat(Contact): contact page added

--- src/Components/Main/form/Form.jsx
+++ src/Components/Main/form/Form.jsx
@@ -1,22 +1,46 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useAppContext } from '../../../hooks/useAppContext';
 import { Input } from './Input';
 import { Label } from './Label';
 
-const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
-  const [formValues, setFormValues] = useState({
-    name: { value: '', errors: null, validationClass: '' },
-    email: { value: '', errors: null, validationClass: '' },
-    isFormValid: false
-  });
-
+const Form = ({ formValues, setFormValues }) => {
   const {
     state: { isDarkMode },
   } = useAppContext();
 
   const onInputChange = e => {
-    setFormValues({ ...formValues, [e.target.name]: { ...formValues[e.target.name], value: e.target.value } });
+    const value = e.target.value;
+    const name = e.target.name;
+    setFormValues({ ...formValues, [name]: { ...formValues[name], value } });
+
+    if (name === 'name') {
+      if (value.trim().length > 5) {
+        setFormValues({
+          ...formValues,
+          name: {
+            ...formValues['name'],
+            value,
+            errors: false,
+            validationClass: 'app__contact__form__input_success',
+          },
+        });
+      }
+    }
+    if (name === 'email') {
+      const emailRegex = new RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
+      const isValidEmail = emailRegex.test(value);
+      if (isValidEmail) {
+        setFormValues({
+          ...formValues,
+          email: {
+            ...formValues['email'],
+            value,
+            errors: false,
+            validationClass: 'app__contact__form__input_success',
+          },
+        });
+      }
+    }
   };
 
   const onInputBlur = e => {
@@ -24,7 +48,7 @@ const Form = () => {
     const name = e.target.name;
 
     if (name === 'name') {
-      if (value.length <= 5) {
+      if (value.trim().length <= 5) {
         setFormValues({
           ...formValues,
           name: {
@@ -35,15 +59,6 @@ const Form = () => {
         });
         return;
       }
-
-      setFormValues({
-        ...formValues,
-        name: {
-          ...formValues['name'],
-          errors: false,
-          validationClass: 'app__contact__form__input_success',
-        },
-      });
     }
 
     if (name === 'email') {
@@ -55,89 +70,95 @@ const Form = () => {
           ...formValues,
           email: {
             ...formValues['email'],
-            errors: 'Email is invalid.',
+            errors: 'The email is invalid.',
             validationClass: 'app__contact__form__input_error',
           },
         });
         return;
       }
-
-      setFormValues({
-        ...formValues,
-        email: {
-          ...formValues['email'],
-          errors: false,
-          validationClass: 'app__contact__form__input_success',
-        },
-      });
     }
-    
-    if(formValues.name.errors === false && formValues.email.errors === false){
-      setFormValues({...formValues, isFormValid: true});
-      return;
-    }
+  };
 
-    
+  const onFormSubmit = e => {
+    e.preventDefault();
+
+    if (
+      formValues.email.errors ||
+      formValues.name.errors ||
+      formValues.email.value === '' ||
+      formValues.name.value === ''
+    ) {
+      setFormValues({ ...formValues, isFormSubmitted: true, isSuccess: false });
+    } else {
+      setFormValues({ ...formValues, isFormSubmitted: true, isSuccess: true });
+      console.log('Name:', formValues.name.value);
+      console.log('Email:', formValues.email.value);
+    }
+    return;
   };
 
   const formThemeClass = isDarkMode ? 'dark' : 'light';
   const submitInputThemeClass = isDarkMode ? 'mainDark' : 'mainLight';
 
   return (
-    <div>
-      <form className={`app__contact__form ${formThemeClass}`}>
-        <fieldset className="app__contact__form__fieldset">
-          <legend className="app__contact__form__legend">Contact information</legend>
-          <Label
-            labelClass="app__contact__form__label"
-            htmlFor="name"
-            text="Name"
-          />
-          <Input
-            inputClass={`app__contact__form__input ${formValues.name.validationClass}`}
-            onChange={onInputChange}
-            type="text"
-            id="name"
-            name="name"
-            value={formValues.name.value}
-            placeholder="Enter your name"
-            onBlur={onInputBlur}
-            isErrors={formValues.name.errors}
-          />
-          {formValues.name.errors && (
-            <span className="app__contact__form__error_message">{formValues.name.errors}</span>
-          )}
-
-          <Label
-            labelClass="app__contact__form__label"
-            htmlFor="email"
-            text="Email"
-          />
-          <Input
-            inputClass={`app__contact__form__input ${formValues.email.validationClass}`}
-            onChange={onInputChange}
-            type="email"
-            id="email"
-            name="email"
-            value={formValues.email.value}
-            placeholder="Enter your email"
-            onBlur={onInputBlur}
-            isErrors={formValues.email.errors}
-          />
-          {formValues.email.errors && (
-            <span className="app__contact__form__error_message">{formValues.email.errors}</span>
-          )}
-        </fieldset>
-
-        <Input
-          inputClass={`${submitInputThemeClass} app__contact__form__submit`}
-          type="submit"
-          value="Submit"
-          isErrors={null}
-          isFormValid={formValues.isFormValid}
+    <form
+      className={`app__contact__form ${formThemeClass}`}
+      onSubmit={onFormSubmit}
+    >
+      <fieldset className="app__contact__form__fieldset">
+        <legend className="app__contact__form__legend">Contact information</legend>
+        <Label
+          labelClass="app__contact__form__label"
+          htmlFor="name"
+          text="Name"
         />
-      </form>
-    </div>
+        <Input
+          inputClass={`app__contact__form__input ${formValues.name.validationClass}`}
+          onChange={onInputChange}
+          type="text"
+          id="name"
+          name="name"
+          value={formValues.name.value}
+          placeholder="Enter your name"
+          onBlur={onInputBlur}
+          isErrors={formValues.name.errors}
+        />
+        {formValues.name.errors && (
+          <span className="app__contact__form__input_error_message">{formValues.name.errors}</span>
+        )}
+
+        <Label
+          labelClass="app__contact__form__label"
+          htmlFor="email"
+          text="Email"
+        />
+        <Input
+          inputClass={`app__contact__form__input ${formValues.email.validationClass}`}
+          onChange={onInputChange}
+          type="email"
+          id="email"
+          name="email"
+          value={formValues.email.value}
+          placeholder="Enter your email"
+          onBlur={onInputBlur}
+          isErrors={formValues.email.errors}
+        />
+        {formValues.email.errors && (
+          <span className="app__contact__form__input_error_message">{formValues.email.errors}</span>
+        )}
+      </fieldset>
+      <Input
+        inputClass={`${submitInputThemeClass} app__contact__form__submit`}
+        type="submit"
+        value="Submit"
+        isErrors={null}
+        isFormValid={formValues.isFormValid}
+      />
+
+      {formValues.isFormSubmitted && !formValues.isSuccess && (
+        <span className="app__contact__form__error_message">Please check your information again.</span>
+      )}
+    </form>
   );
 };
 
--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LeftArrow } from '../Components/icons/LeftArrow';
 import Form from '../Components/Main/form/Form';
+import { CheckCircleFillIcon } from '../Components/icons/CheckCircleFillIcon';
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Contact = () => {
   const navigate = useNavigate();
-  
+
+  const [formValues, setFormValues] = useState({
+    name: { value: '', errors: null, validationClass: '' },
+    email: { value: '', errors: null, validationClass: '' },
+    isFormSubmitted: false,
+    isSuccess: false,
+  });
+
   return (
     <>
-    <div onClick={() => navigate(-1)}>
-        <LeftArrow arrowClass="app__main__card-grid__arrow" />
-      </div>
-      <div className="app__contact__text">
-        <h2>Want to know more?</h2>
-        <p>Send us your questions and we will contact you</p>
-      </div>
+      <div className="app__contact__container">
+        <div onClick={() => navigate(-1)}>
+          <LeftArrow arrowClass="app__main__card-grid__arrow" />
+        </div>
+        {formValues.isSuccess ? (
+          <div className="app__contact__text_success">
+            <CheckCircleFillIcon />
+            <h2>Thanks {formValues.name.value}</h2>
+            <p>We will contact you soon, through email.</p>
+          </div>
+        ) : (
+          <>
+            <div className="app__contact__text">
+              <h2>Want to know more?</h2>
+              <p>Send us your questions and we will contact you</p>
+            </div>
 
-      <Form />
+            <Form
+              formValues={formValues}
+              setFormValues={setFormValues}
+            />
+          </>
+        )}
+      </div>
     </>
   );
 };
